e43d4bec07a151d4f7247c4a0c704fbda385edc7
diepes <pieter.smit@themarket.nz>
2023-05-19 00:57:13 +1200

    main.rs json output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate sys_info_rs;
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    //let mut hostname: String;
+    //std::fs::File::open("/proc/sys/kernel/hostname").expect("Err reading proc").read_to_string(&mut hostname).expect("Err reading hostname");
     let info: SysInfo = SysInfo {
         loadavg: sys_info_rs::loadavg().expect("Err getting loadavg"),
         disk_info: sys_info_rs::disk_info().expect("Err getting disk info"),
@@ -10,18 +12,28 @@ fn main() {
             .expect("Err getting boottime")
             .tv_sec,
         hostname: sys_info_rs::hostname().expect("Err getting hostname)"),
+        //hostname: hostname,
+        time: std::time::SystemTime::now()
+            .duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .expect("Err getting unix time.")
+            .as_secs(),
     };
 
-    println!("info {:#?} ", info);
+    //println!("info {:#?} ", info);
     // println!("disk_info {:?} sec", sys_info_rs::disk_info().unwrap());
     // println!("loadavg {:?}", sys_info_rs::loadavg().unwrap());
+    let info_json = serde_json::json!(info); //.expect("Err not json.");
+    println!("{}", info_json);
+
+    //println!("OS={}", std::env::consts::OS);
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, serde::Serialize)]
 struct SysInfo {
     loadavg: sys_info_rs::LoadAvg,
     disk_info: sys_info_rs::DiskInfo,
     mem_info: sys_info_rs::MemInfo,
     boottime_sec: i64,
     hostname: String,
+    time: u64,
 }
